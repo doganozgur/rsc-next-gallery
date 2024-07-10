ba9a963e47fc2ae35d3a5c78b01aa9866ebd98e8
Dogan Ozgur <doganozgur034@gmail.com>
2024-07-10 16:32:28 +0300

    scaffold ui

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <div className="flex w-full items-center justify-between border-b p-4 font-semibold">
+      <h1 className="text-3xl font-bold">Gallery</h1>
+      <p>Sign In</p>
+    </div>
+  );
+};
+
+export default Header;
